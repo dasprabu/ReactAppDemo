e575402b9320767833969396bc766c78ac62e84a
Prabu Das Natarajan <dasprabu@gmail.com>
2019-08-03 12:18:10 +0800

    added image

--- app/index.js
+++ app/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Bootstrap from 'bootstrap'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Background  from './Images/watch.jpeg';
+
+
+
+
 
 
 class App extends React.Component{
     render(){
+
+
         return(
-<div>
-    <div className="bg-danger text-center">
+<div className="row">
+    <div className="col-sm-12 bg-danger text-center">
         <h1>My First Bootstrap Pageee</h1>
         <p>Resize this responsive page to see the effect!</p>
     </div>
